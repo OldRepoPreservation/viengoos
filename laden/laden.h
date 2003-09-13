db3bd625f20de535bcab489d39acfe635f7bd595
marcus <marcus>
2003-09-13 00:25:50 +0000

    Add libc-parts, which takes functions out of libc.a bit by bit.

--- laden/laden.h
+++ laden/laden.h
@@ -18,8 +18,14 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
 
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+
 #include <l4.h>
-#include "string.h"
+
 #include "output.h"
 #include "shutdown.h"
 #include "loader.h"
