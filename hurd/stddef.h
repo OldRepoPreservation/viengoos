abd5f5b37175397568c948002385f734d5f1ecbc
neal <neal>
2008-01-01 16:18:30 +0000

    2008-01-01  Neal H. Walfield  <neal@gnu.org>
    	* stddef.h (do_debug) [DEBUG_ELIDE]: Make a proper conditional.
    	(debug): Don't change definition depending on DEBUG_ELIDE, always
    	use the full definition relying on do_debug to do the right thing.

--- hurd/stddef.h
+++ hurd/stddef.h
@@ -1,23 +1,22 @@
 /* hurd/stddef.h - Standard definitions for the GNU Hurd.
-   Copyright (C) 2007 Free Software Foundation, Inc.
+   Copyright (C) 2007, 2008 Free Software Foundation, Inc.
    Written by Neal H. Walfield <neal@gnu.org>
 
    This file is part of the GNU Hurd.
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+   GNU Hurd is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful, but
+   GNU Hurd is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with this program; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA.  */
+   License along with GNU Hurd.  If not, see
+   <http://www.gnu.org/licenses/>.  */
 
 #ifndef _HURD_STDDEF_H
 #define _HURD_STDDEF_H	1
@@ -26,15 +25,21 @@
 #include <assert.h>
 
 /* Convenient debugging macros.  */
-#ifndef DEBUG_ELIDE
+#ifdef DEBUG_ELIDE
 
-#ifndef DEBUG_COND
+# define do_debug(level) if (0)
+
+#else
+
+# ifndef DEBUG_COND
 /* The default DEBUG_COND is LEVEL <= output_debug.  */
 extern int output_debug;
-# define DEBUG_COND(level) ((level) <= output_debug)
-#endif
+#  define DEBUG_COND(level) ((level) <= output_debug)
+# endif
 
-#define do_debug(level) if (DEBUG_COND(level))
+# define do_debug(level) if (DEBUG_COND(level))
+
+#endif /* DEBUG_ELIDE */
 
 #include <l4/thread.h>
 
@@ -49,10 +54,6 @@ extern int output_debug;
 	        ##__VA_ARGS__);					\
     }								\
   while (0)
-#else
-#define do_debug(level) do {} while (0)
-#define debug(level, fmt, ...) do {} while (0)
-#endif
 
 /* Print an error message and fail.  This function must be provided by
    the run-time.  */
