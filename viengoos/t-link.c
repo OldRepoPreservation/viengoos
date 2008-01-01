ec47d23d010eae4bfbe1e775fb4c628c65493b63
neal <neal>
2008-01-01 15:37:26 +0000

    2008-01-01  Neal H. Walfield  <neal@gnu.org>
    	* t-link.c: Include <stdlib.h>.
    	(test): Use malloc to allocate the object.  Free them on unlink.
    	Improve coverage of tail and prev functions.

--- viengoos/t-link.c
+++ viengoos/t-link.c
@@ -1,6 +1,7 @@
 #define _L4_TEST_MAIN
 #include "t-environment.h"
 
+#include <stdlib.h>
 #include <hurd/stddef.h>
 #include "list.h"
 #include "output.h"
@@ -19,7 +20,7 @@ int output_debug = 0;
 void
 test (void)
 {
-  struct object_desc descs[N];
+  struct object_desc *descs[N];
 
   struct object_activity_lru_list list;
   object_activity_lru_list_init (&list);
@@ -33,15 +34,21 @@ test (void)
     {
       assert (object_activity_lru_list_count (&list) == i);
 
-      descs[i].age = i;
-      descs[i].activity_lru.next = descs[i].activity_lru.prev = NULL;
-      object_activity_lru_list_push (&list, &descs[i]);
+      descs[i] = calloc (sizeof (struct object_desc), 1);
+      descs[i]->age = i;
+      object_activity_lru_list_push (&list, descs[i]);
 
       int j = i;
       for (p = object_activity_lru_list_head (&list);
 	   p; p = object_activity_lru_list_next (p))
 	assert (p->age == j --);
       assert (j == -1);
+
+      j = 0;
+      for (p = object_activity_lru_list_tail (&list);
+	   p; p = object_activity_lru_list_prev (p))
+	assert (p->age == j ++);
+      assert (j == i + 1);
     }
   assert (object_activity_lru_list_count (&list) == i);
 
@@ -49,7 +56,8 @@ test (void)
      -> 1.)  */
   for (i = 0; i < N; i += 2)
     {
-      object_activity_lru_list_unlink (&list, &descs[i]);
+      object_activity_lru_list_unlink (&list, descs[i]);
+      free (descs[i]);
 
       int j = N - 1;
       for (p = object_activity_lru_list_head (&list);
@@ -65,7 +73,8 @@ test (void)
   /* Unlink nodes 1, 3, 5, 7, 9.  (Leaving an empty list.)  */
   for (i = 1; i < N; i += 2)
     {
-      object_activity_lru_list_unlink (&list, &descs[i]);
+      object_activity_lru_list_unlink (&list, descs[i]);
+      free (descs[i]);
 
       int j = N - 1;
       for (p = object_activity_lru_list_head (&list);
@@ -81,9 +90,15 @@ test (void)
   struct object_activity_lru_list a;
   object_activity_lru_list_init (&a);
 
-  object_activity_lru_list_push (&a, &descs[3]);
-  object_activity_lru_list_push (&a, &descs[2]);
-  object_activity_lru_list_push (&a, &descs[1]);
+  descs[3] = calloc (sizeof (struct object_desc), 1);
+  descs[3]->age = 3;
+  object_activity_lru_list_push (&a, descs[3]);
+  descs[2] = calloc (sizeof (struct object_desc), 1);
+  descs[2]->age = 2;
+  object_activity_lru_list_push (&a, descs[2]);
+  descs[1] = calloc (sizeof (struct object_desc), 1);
+  descs[1]->age = 1;
+  object_activity_lru_list_push (&a, descs[1]);
 
   assert (object_activity_lru_list_count (&a) == 3);
 
@@ -91,9 +106,15 @@ test (void)
   struct object_activity_lru_list b;
   object_activity_lru_list_init (&b);
 
-  object_activity_lru_list_push (&b, &descs[6]);
-  object_activity_lru_list_push (&b, &descs[5]);
-  object_activity_lru_list_push (&b, &descs[4]);
+  descs[6] = calloc (sizeof (struct object_desc), 1);
+  descs[6]->age = 6;
+  object_activity_lru_list_push (&b, descs[6]);
+  descs[5] = calloc (sizeof (struct object_desc), 1);
+  descs[5]->age = 5;
+  object_activity_lru_list_push (&b, descs[5]);
+  descs[4] = calloc (sizeof (struct object_desc), 1);
+  descs[4]->age = 4;
+  object_activity_lru_list_push (&b, descs[4]);
 
   assert (object_activity_lru_list_count (&b) == 3);
 
@@ -125,7 +146,8 @@ test (void)
   /* Remove some elements.  */
   for (i = 2; i < 7; i ++)
     {
-      object_activity_lru_list_unlink (&b, &descs[i]);
+      object_activity_lru_list_unlink (&b, descs[i]);
+      free (descs[i]);
 
       assert (object_activity_lru_list_head (&b)->age == 1);
 
