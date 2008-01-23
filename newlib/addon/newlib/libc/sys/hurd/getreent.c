707d4972feeb5f04b59167d7cd6cf0f6f29a6316
neal <neal>
2008-01-23 23:20:25 +0000

    2008-01-24  Neal H. Walfield  <neal@gnu.org>
    	* addon/newlib/libc/sys/hurd/getreent.c (reent_main): New
    	variable.
    	(reent_main_alloced): Likewise.
    	(reent_key_destroy): Clear utcb[_L4_UTCB_THREAD_WORD0].
    	(__getreent): If REENT_MAIN_ALLOCED is not true, then use
    	REENT_MAIN.  Otherwise, use the slab.  Only initialize the key if
    	we use the slab and then, only after we've initialized the new
    	structure and stashed it in utcb[_L4_UTCB_THREAD_WORD0].

--- newlib/addon/newlib/libc/sys/hurd/getreent.c
+++ newlib/addon/newlib/libc/sys/hurd/getreent.c
@@ -1,3 +1,21 @@
+/* getreent.c - Reentrancy support.
+   Copyright (C) 2008 Free Software Foundation, Inc.
+   Written by Neal H. Walfield <neal@gnu.org>.
+
+   GNU Hurd is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with GNU Hurd.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
 #include <l4/thread.h>
 #include <hurd/slab.h>
 #include <hurd/storage.h>
@@ -27,6 +45,11 @@ slab_dealloc (void *hook, void *buffer, size_t size)
   return 0;
 }
 
+/* We don't use the slab for the first thread as this wastes space and
+   we don't have enough stack when we first allocate it.  */
+static struct _reent reent_main;
+static int reent_main_alloced;
+
 /* Storage descriptors are alloced from a slab.  */
 static struct hurd_slab_space reent_slab
   = HURD_SLAB_SPACE_INITIALIZER (struct _reent, slab_alloc, slab_dealloc,
@@ -41,11 +64,20 @@ static pthread_key_t reent_key;
 static void
 reent_key_destroy (void *data)
 {
+  _L4_word_t *utcb = _L4_utcb ();
+  assert (utcb[_L4_UTCB_THREAD_WORD0] == data);
+
   struct _reent *reent = data;
 
-  /* XXX: This may be wrong: if hurd_slab_dealloc needs to use reent,
-     we lose.  */
+  /* XXX: This may be wrong: we know that hurd_slab_dealloc does not
+     need to use reent, however, other keys may be freed, in which
+     case, we might lose.  But it will cause a new TSD to be created
+     and then freed.  The number of times this can happen is bounded.
+     In the very worst case, we might leak some memory.  */
   _reclaim_reent (reent);
+
+  utcb[_L4_UTCB_THREAD_WORD0] = 0;
+
   hurd_slab_dealloc (&reent_slab, reent);
 }
 
@@ -65,18 +97,31 @@ __getreent (void)
   if (unlikely (! utcb[_L4_UTCB_THREAD_WORD0]))
     /* Thread doesn't have a reent structure.  Allocate one.  */
     {
-      pthread_once (&reent_key_init, reent_key_alloc);
-
       void *buffer;
-      error_t err = hurd_slab_alloc (&reent_slab, &buffer);
-      if (err)
-	panic ("Out of memory!");
+      if (! reent_main_alloced)
+	{
+	  buffer = &reent_main;
+	  reent_main_alloced = 1;
+	}
+      else
+	{
+	  error_t err = hurd_slab_alloc (&reent_slab, &buffer);
+	  if (err)
+	    panic ("Out of memory!");
+	}
 
-      pthread_setspecific (reent_key, buffer);
+      struct _reent *reent = buffer;
+      _REENT_INIT_PTR (reent);
 
       utcb[_L4_UTCB_THREAD_WORD0] = (uintptr_t) buffer;
+
+      if (buffer != &reent_main)
+	/* Only call after we've setup the reent structure.  */
+	{
+	  pthread_once (&reent_key_init, reent_key_alloc);
+	  pthread_setspecific (reent_key, buffer);
+	}
     }
 
   return (void *) utcb[_L4_UTCB_THREAD_WORD0];
 }
-
