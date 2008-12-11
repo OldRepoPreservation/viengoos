704080f3e67bd2311508e5632f68ef1f41f91ac5
Neal H. Walfield <neal@gnu.org>
2008-12-11 17:18:33 +0100

    Create backtrace.h.
    2008-12-11  Neal H. Walfield  <neal@gnu.org>
    
    	* backtrace.h: New file.

---
+++ libc-parts/backtrace.h
@@ -0,0 +1,26 @@
+/* backtrace.h - Gather a backtrace.
+   Copyright (C) 2008 Free Software Foundation, Inc.
+   Written by Neal H. Walfield <neal@gnu.org>.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef _BACKTRACE_H
+#define _BACKTRACE_H
+
+extern int backtrace (void **array, int size);
+
+extern void backtrace_print (void);
+
+#endif
