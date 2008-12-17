4de91cdeafc5566289af7b0024c292207bc1eef9
Neal H. Walfield <neal@gnu.org>
2008-12-17 15:15:50 +0100

    Update viengoos.h to include all viengoos headers.
    2008-12-17  Neal H. Walfield  <neal@gnu.org>
    
    	* viengoos.h: Include <viengoos/activity.h>, <viengoos/addr.h>,
    	<viengoos/addr-trans.h>, <viengoos/cap.h>, <viengoos/folio.h>,
    	<viengoos/futex.h>, <viengoos/ipc.h>, <viengoos/math.h>,
    	<viengoos/message.h>, <viengoos/messenger.h>, and
    	<viengoos/misc.h>.

--- libviengoos/viengoos.h
+++ libviengoos/viengoos.h
@@ -1,1 +1,37 @@
+/* viengoos.h - Viengoos definitions.
+   Copyright (C) 2007, 2008 Free Software Foundation, Inc.
+   Written by Neal H. Walfield <neal@gnu.org>.
+
+   This file is part of the GNU Hurd.
+
+   GNU Hurd is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with GNU Hurd.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef _VIENGOOS_H
+#define _VIENGOOS_H 1
+
+#include <viengoos/activity.h>
+#include <viengoos/addr.h>
+#include <viengoos/addr-trans.h>
+#include <viengoos/cap.h>
+#include <viengoos/folio.h>
+#include <viengoos/futex.h>
+#include <viengoos/ipc.h>
+#include <viengoos/math.h>
+#include <viengoos/message.h>
+#include <viengoos/messenger.h>
+#include <viengoos/misc.h>
 #include <viengoos/thread.h>
+
+#endif
